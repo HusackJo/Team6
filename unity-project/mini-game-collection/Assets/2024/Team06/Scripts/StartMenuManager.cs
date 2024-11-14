5131fec32a936b85b7f3801c18414f6b287f78bc
HusackJo <husackjo@gmail.com>
2024-11-14 15:50:50 -0500

    Revert "Checking..."
    This reverts commit 4f0f0ce27d969675440bbed37ce9fd51dd371b6f.

--- unity-project/mini-game-collection/Assets/2024/Team06/Scripts/StartMenuManager.cs
+++ unity-project/mini-game-collection/Assets/2024/Team06/Scripts/StartMenuManager.cs
@@ -7,9 +7,6 @@ namespace MiniGameCollection.Games2024.Team06
 {
     public class StartMenuManager : MonoBehaviour
     {
-        public void GoToPlayScene()
-        {
-            SceneManager.LoadScene("Team 6 Main Scene");
-        }
+
     }
 }
